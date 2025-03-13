0b999e5861ffda725099ec6debdac5255b70e4f8
RJSP34 <139016586+RJSP34@users.noreply.github.com>
2025-03-13 00:34:52 +0000

    Update network.ps1

--- scripts/src/network.ps1
+++ scripts/src/network.ps1
@@ -1,11 +1,11 @@
-﻿$REST_API_HOST = "localhost"
-$REST_API_PORT = 8888
-$P2P_GATEWAY_HOST = "localhost"
-$P2P_GATEWAY_PORT = 8080
+﻿$REST_API_HOST = ""
+$REST_API_PORT = 
+$P2P_GATEWAY_HOST = ""
+$P2P_GATEWAY_PORT = 
 
 $REST_API_URL="https://${REST_API_HOST}:${REST_API_PORT}/api/v5_1"
-$REST_API_USER = "admin"
-$REST_API_PASSWORD = "admin"
+$REST_API_USER = ""
+$REST_API_PASSWORD = ""
 $AUTH_INFO = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("${REST_API_USER}:${REST_API_PASSWORD}" -f $username,$password)))
 
 $WORK_DIR = "creating-mgm-cpi"
